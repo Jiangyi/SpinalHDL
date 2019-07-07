717f79eecef1f94de603a2076ca3ac9fa48d5890
Andrew Jiang <andrew.jiang@edu.uwaterloo.ca>
2019-07-07 01:07:33 -0400

    HexTools: Convert everything to Long
    Since unsigned ints are not a thing in Scala, anything larger than
    0x7FFFFFFF will be treated as a negative number, which is undesirable.
    Use longs (64-bit) which will give us 2^32 space for positive values.

--- lib/src/main/scala/spinal/lib/misc/HexTools.scala
+++ lib/src/main/scala/spinal/lib/misc/HexTools.scala
@@ -3,25 +3,25 @@ package spinal.lib.misc
 import spinal.core.{Data, Mem}
 
 object HexTools{
-  def readHexFile(path : String, hexOffset : Int, callback : (Int, Int) => Unit) : Unit ={
+  def readHexFile(path : String, hexOffset : Long, callback : (Long, Long) => Unit) : Unit ={
     import scala.io.Source
-    def hToI(that : String, start : Int, size : Int) = Integer.parseInt(that.substring(start,start + size), 16)
+    def hToL(that : String, start : Int, size : Int) = java.lang.Long.parseLong(that.substring(start,start + size), 16)
 
-    var offset = 0
+    var offset: Long = 0
     for (line <- Source.fromFile(path).getLines) {
       if (line.charAt(0) == ':'){
-        val byteCount = hToI(line, 1, 2)
-        val nextAddr = hToI(line, 3, 4) + offset
-        val key = hToI(line, 7, 2)
+        val byteCount = hToL(line, 1, 2)
+        val nextAddr = hToL(line, 3, 4) + offset
+        val key = hToL(line, 7, 2)
         key match {
           case 0 =>
-            for(i <- 0 until byteCount){
-              callback(nextAddr + i - hexOffset, hToI(line, 9 + i * 2, 2))
+            for(i <- 0 until byteCount.toInt){
+              callback(nextAddr + i - hexOffset, hToL(line, 9 + i * 2, 2))
             }
           case 2 =>
-            offset = hToI(line, 9, 4) << 4
+            offset = hToL(line, 9, 4) << 4
           case 4 =>
-            offset = hToI(line, 9, 4) << 16
+            offset = hToL(line, 9, 4) << 16
           case 3 =>
           case 5 =>
           case 1 =>
@@ -30,17 +30,17 @@ object HexTools{
     }
   }
 
-  def readHexFile(path : String, hexOffset : Int): Array[BigInt] ={
-    var onChipRomSize = 0
+  def readHexFile(path : String, hexOffset : Long): Array[BigInt] ={
+    var onChipRomSize: Long = 0
     readHexFile(path, hexOffset ,(address, _) => {
-      onChipRomSize = Math.max((address).toInt, onChipRomSize) + 1
+      onChipRomSize = Math.max(address, onChipRomSize) + 1
     })
 
-    val initContent = Array.fill[BigInt]((onChipRomSize+3)/4)(0)
+    val initContent = Array.fill[BigInt](((onChipRomSize+3)/4).toInt)(0)
     readHexFile(path, hexOffset,(address,data) => {
-      val addressWithoutOffset = (address).toInt
+      val addressWithoutOffset: Long = address
       if(addressWithoutOffset < onChipRomSize)
-        initContent(addressWithoutOffset >> 2) |= BigInt(data) << ((addressWithoutOffset & 3)*8)
+        initContent((addressWithoutOffset >> 2).toInt) |= BigInt(data) << ((addressWithoutOffset & 3)*8).toInt
     })
     initContent
   }
@@ -48,8 +48,8 @@ object HexTools{
   def initRam[T <: Data](ram : Mem[T], onChipRamHexFile : String, hexOffset : BigInt): Unit ={
     val initContent = Array.fill[BigInt](ram.wordCount)(0)
     HexTools.readHexFile(onChipRamHexFile, 0,(address,data) => {
-      val addressWithoutOffset = (address - hexOffset).toInt
-      initContent(addressWithoutOffset >> 2) |= BigInt(data) << ((addressWithoutOffset & 3)*8)
+      val addressWithoutOffset: Long = (address - hexOffset).toLong
+      initContent((addressWithoutOffset >> 2).toInt) |= BigInt(data) << ((addressWithoutOffset & 3)*8).toInt
     })
     ram.initBigInt(initContent)
   }
